0b525f6d3a3f20a32afc1a99a49461c20fae3f8f
徐晶 <xujing@jxulife.com>
2020-05-09 22:37:55 +0800

    初始化代码

---
+++ clean_log.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#clean app log
+
+sExecCurrentDir=$(cd "$(dirname "$0")"; /bin/pwd)
+sExecRootDir=`/bin/readlink -f $sExecCurrentDir/`
+
+source $sExecRootDir'/inc/initial.sh'
+
+sCleanPattern=$1;
+
+if [ -z $sCleanPattern ]; then
+    showError 'We need clean pattern.'
+else
+    showInfo 'We will clean app log. Pattern is '$sCleanPattern
+fi 
+
+$sDeployServer='all_servers'
+
+_sYamlParam="sDeployServer=$sDeployServer sDeployUser=$sDeployUser sDeleteRootDir=$sLogInstallRootDir/app/laravel/storage-release sDeletePattern=$sCleanPattern"
+$sExecAnsiblePlaybook -v -i "$sExecRootDir"/hosts -e "$_sYamlParam" "$sExecRootDir"/yaml/find_delete_dir.yaml
